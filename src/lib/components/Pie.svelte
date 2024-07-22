1e2cd8748f89ba51812735f5493d741dec6fdf2e
ccalobeto <calobeto.leon@gmail.com>
2024-07-22 15:37:25 -0500

    6.2.1 Adding labels to our data

--- src/lib/components/Pie.svelte
+++ src/lib/components/Pie.svelte
@@ -1,7 +1,14 @@
 <script>
   import * as d3 from "d3";
 
-  let data = [1, 2, 3, 4, 5, 5];
+  let data = [
+    { value: 1, label: "apples" },
+    { value: 2, label: "oranges" },
+    { value: 3, label: "mangos" },
+    { value: 4, label: "pears" },
+    { value: 5, label: "limes" },
+    { value: 5, label: "cherries" },
+  ];
   let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
   let sliceGenerator = d3.pie();
   let arcData = sliceGenerator(data);
