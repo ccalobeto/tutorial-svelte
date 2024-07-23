40fa4d680cf82553583bf41aded31ed6b2e7cc19
ccalobeto <calobeto.leon@gmail.com>
2024-07-23 10:26:58 -0500

    6.3 Plotting our actual data

--- src/lib/components/Pie.svelte
+++ src/lib/components/Pie.svelte
@@ -1,14 +1,7 @@
 <script>
   import * as d3 from "d3";
+  export let data = [];
 
-  let data = [
-    { value: 1, label: "apples" },
-    { value: 2, label: "oranges" },
-    { value: 3, label: "mangos" },
-    { value: 4, label: "pears" },
-    { value: 5, label: "limes" },
-    { value: 5, label: "cherries" },
-  ];
   let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
   let sliceGenerator = d3.pie().value((d) => d.value);
   let arcData = sliceGenerator(data);
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,31 @@
 <script>
+  import * as d3 from "d3";
   import projects from "$lib/projects.json";
   import Project from "$lib/components/Project.svelte";
   import Pie from "$lib/components/Pie.svelte";
+
+  let rolledData = d3.rollups(
+    projects,
+    (d) => d.length,
+    (d) => d.year,
+  );
+  let pieData = rolledData.map(([year, count]) => {
+    return { value: count, label: year };
+  });
+
+  let query = "";
+  let filteredProjects;
+
+  $: filteredProjects = projects.filter((project) => {
+    if (query) {
+      return project.title.includes(query);
+    }
+
+    return true;
+  });
 </script>
 
-<Pie />
+<Pie data={pieData} />
 
 <svelte:head>
   <title>Projects</title>
