4eea101e8e98b86ee2dec4b33d1048581977d92b
ccalobeto <calobeto.leon@gmail.com>
2024-07-24 16:08:27 -0500

    7.1.1 Visualizing quantitative data with D3 - load csv file

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -1,2 +1,22 @@
+<script>
+  import * as d3 from "d3";
+  import { onMount } from "svelte";
+
+  let data = [];
+
+  onMount(async () => {
+    data = await d3.csv("loc.csv", (row) => ({
+      ...row,
+      line: +row.line,
+      depth: +row.depth,
+      length: +row.length,
+      date: new Date(row.date + "T00:00" + row.timezone),
+      datetime: new Date(row.datetime),
+    }));
+    console.log(data);
+  });
+</script>
+
 <h1>Meta</h1>
-<p>This page includes stats about the site</p>
+<p>This page includes stats about the project.</p>
+<p>Total lines of code: {data.length}</p>
