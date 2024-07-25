fbf987ad5b0dea87584aeae567667e2c4b5cd1f8
ccalobeto <calobeto.leon@gmail.com>
2024-07-25 10:40:49 -0500

    7.1.2 Displaying summary stats - transforming commit data

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -3,6 +3,7 @@
   import { onMount } from "svelte";
 
   let data = [];
+  let commits = [];
 
   onMount(async () => {
     data = await d3.csv("loc.csv", (row) => ({
@@ -13,7 +14,38 @@
       date: new Date(row.date + "T00:00" + row.timezone),
       datetime: new Date(row.datetime),
     }));
-    console.log(data);
+
+    commits = d3
+      .groups(data, (d) => d.commit)
+      .map(([commit, lines]) => {
+        let first = lines[0];
+        let { author, date, time, timezone, datetime } = first;
+        let ret = {
+          id: commit,
+          url: "https://github.com/vis-society/lab-7/commit/" + commit,
+          author,
+          date,
+          time,
+          timezone,
+          datetime,
+          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+          totalLines: lines.length,
+        };
+
+        // Like ret.lines = lines
+        // but non-enumerable so it doesn’t show up in JSON.stringify
+        // 25/07/24: put the entire lines per commit and end of the object commit
+        Object.defineProperty(ret, "lines", {
+          value: lines,
+          configurable: true,
+          writable: true,
+          enumerable: false,
+        });
+
+        return ret;
+      });
+
+    console.log(commits);
   });
 </script>
 
