bce5e73e6c7012590b84b570d55afa4abd86e985
ccalobeto <calobeto.leon@gmail.com>
2024-08-06 17:17:02 -0500

    8.1.0: Setup Mapbox Tool

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+	import mapboxgl from 'mapbox-gl';
+	import '../../node_modules/mapbox-gl/dist/mapbox-gl.css';
+	import { PUBLIC_MAPBOX_ACCESS_TOKEN } from '$env/static/public';
+
+	mapboxgl.accessToken = PUBLIC_MAPBOX_ACCESS_TOKEN;
+</script>
+
 <title>Bike Watch</title>
 <h1>Bike Watch</h1>
 <p>
@@ -5,6 +13,14 @@
 	in the Boston area during different times of the day.
 </p>
 
+{console.log(mapboxgl.accessToken)}
+<div id="map"></div>
+
 <style>
 	@import url('$lib/global.css');
+
+	#map {
+		flex: 1;
+		background: yellowgreen;
+	}
 </style>
