0809ba31d56f00f27f42b7aca59e8b14ec36f71a
ccalobeto <calobeto.leon@gmail.com>
2024-08-06 15:47:41 -0500

    8.0 Geospatial visualizations-setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<title>Bike Watch</title>
+<h1>Bike Watch</h1>
+<p>
+	In this project, we will be building an immersive, interactive map visualization of bike traffic
+	in the Boston area during different times of the day.
+</p>
+
+<style>
+	@import url('$lib/global.css');
+</style>
