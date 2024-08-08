d86c3bc86146bb3caa03a034241fced63f83408d
ccalobeto <calobeto.leon@gmail.com>
2024-08-08 11:05:50 -0500

    8.2.1: Import the data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,21 @@
 	mapboxgl.accessToken =
 		'pk.eyJ1IjoiY2NhbG9iZXRvIiwiYSI6ImNsemhhYzc3NjAyZjcybXEwc3pzbzg5aWcifQ.i8wSRm6K7eYFQAgS6T1W2g';
 
-	onMount(() => {
+	onMount(async () => {
 		let map = new mapboxgl.Map({
 			container: 'map',
 			style: 'mapbox://styles/mapbox/streets-v12',
 			zoom: 12,
 			center: [-71.09415, 42.36027]
+		});
+
+		await new Promise((resolve) => {
+			map.on('load', resolve);
+		});
+
+		map.addSource('bike-routes', {
+			type: 'geojson',
+			data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D'
 		});
 	});
 </script>
