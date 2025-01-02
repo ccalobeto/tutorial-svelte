2e847602605124c4f86f45c71f791fee390d257f
ccalobeto <calobeto.leon@gmail.com>
2025-01-01 21:57:26 -0500

    configure google.config.js for extracting data from doc files and load data

--- google.config.js
+++ google.config.js
@@ -12,5 +12,26 @@ export default [
 		"id": "1jFlS8Ghkat-h2W27Q2YfoXRV5RuNFY_ML6hiirMa6HM",
 		"gid": "0",
 		"filepath": "src/data/authors.csv"
-	}
+	},
+	{
+		"id": "1gTiQmDvQEo5SQ2T6qpp4QzwzEQnETVTEQKFoPpZ5ELs",
+		"filepath": "src/data/home.json"
+	},
+	{
+		"id": "1waYYtAT7ldJSAwF09NEwjLZjM2kYdhHIhXkfU0H7jes",
+		"filepath": "src/data/about.json"
+	},
+	{
+		"id": "1Q11WmISgzDgac2mwNIrGYst25zRlaPOAZjFwGkxf-g0",
+		"filepath": "src/data/faq.json"
+	},
+	{
+		"id": "1DLgj3LKzrbVATT1vfeHL5If_gArjqFTk968bBP2OLKo",
+		"filepath": "src/data/privacy.json"
+	},
+	{
+		"id": "1oc1-q8i1FWhgsZ7tQ6UfjGl3WQ8O57cCFn858qbvATM",
+		"filepath": "src/data/pitch.json"
+	},
+
 ]
