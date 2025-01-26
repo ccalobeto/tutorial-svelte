916cfe18d241d709a0978b632f7010c6915aeee6
ccalobeto <calobeto.leon@gmail.com>
2025-01-25 19:43:19 -0500

    update github pages

--- docs/_app/immutable/chunks/paths.Cc2_0JGx.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_192g9kj)==null?void 0:s.base)??"/svelte-pudding";var e;const a=((e=globalThis.__sveltekit_192g9kj)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ docs/_app/immutable/entry/start.BIqCTbZn.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CNqFwhKn.js";export{t as start};
