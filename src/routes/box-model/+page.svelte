cd4bb6e373548905aa328694b15a2a2eaa293588
ccalobeto <calobeto.leon@gmail.com>
2025-02-16 06:18:34 -0500

    2.1 Box concepts

---
+++ src/routes/box-model/+page.svelte
@@ -0,0 +1,29 @@
+<span> Esto es un texto </span>
+<span> Esto es un texto </span>
+<div class="container">Modelo de la caja</div>
+
+<section class="section-container"></section>
+
+<style>
+	.container {
+		width: 50vw;
+		height: 50vh;
+		background-color: #09f;
+	}
+
+	.section-container {
+		width: 360px;
+		height: 360px;
+		background-color: #09f;
+		padding: 36px;
+		border: 10px solid purple;
+		margin: 20px;
+		/* box-sizing: border-box; /*by default* */
+		box-sizing: border-box;
+	}
+
+	span {
+		background: red;
+		padding: 20px;
+	}
+</style>
