cae550530763101c696b87ccd723823b291bbfd2
ccalobeto <calobeto.leon@gmail.com>
2025-02-16 11:40:44 -0500

    3. z-index

---
+++ src/routes/z-index/+page.svelte
@@ -0,0 +1,31 @@
+<h1>--z-index--</h1>
+<section class="parent-z-index">
+	<div class="container-position-z-index">container</div>
+</section>
+<section class="parent-z-index">
+	<div class="container-position-z-index">container</div>
+</section>
+
+<style>
+	/* z-index */
+	.parent-z-index {
+		border: 5px solid #000;
+		width: 750px;
+		height: 800px;
+		box-sizing: border-box;
+		/* position: relative; wthis is the key, must exists */
+	}
+
+	.container-position-z-index {
+		width: 100px;
+		height: 100px;
+		background: #09f;
+		position: fixed;
+		right: 0;
+		z-index: 1;
+	}
+	h1 {
+		color: gray;
+		opacity: 0.5;
+	}
+</style>
