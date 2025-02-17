bc4c34bc3d1d9b377dffaaed1caaf2826dfd56e2
ccalobeto <calobeto.leon@gmail.com>
2025-02-17 09:17:08 -0500

    5.2 Bento Grids

--- src/routes/grid/+page.svelte
+++ src/routes/grid/+page.svelte
@@ -29,7 +29,40 @@
 	<img src="images/pic05.jpg" alt="" />
 </div>
 
+<h1>--Grid Bento--</h1>
+<section class="container-grid-bento">
+	<div>1</div>
+	<div>2</div>
+	<div>3</div>
+	<div>4</div>
+	<div>5</div>
+	<div>6</div>
+	<div>7</div>
+</section>
+
 <style>
+	/* Grid Bento */
+	.container-grid-bento {
+		background: lightsalmon;
+		border: 3px solid #000;
+		border-radius: 10px;
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+	}
+
+	.container-grid-bento div {
+		background: lightblue;
+		border: 2px solid #09f;
+		border-radius: 6px;
+	}
+
+	.container-grid-bento div:first-child {
+		background: lightgreen;
+		border: 2px solid green;
+		grid-column: 1 / 2; /* start column / end column */
+		grid-row: 1 / 3; /* start row / end row */
+	}
+
 	/* Grid example */
 	.grid-images {
 		display: grid;
