6ba59e50b5ba94314f268c397254eea22646d417
ccalobeto <calobeto.leon@gmail.com>
2025-02-17 13:19:26 -0500

    7.2 Animations

--- src/routes/animations/+page.svelte
+++ src/routes/animations/+page.svelte
@@ -1,19 +1,98 @@
-<h1>--Animation-Initial state--</h1>
+<h1>--Transitions-initial state--</h1>
 <section>
 	<div class="pulser"></div>
 </section>
 
-<h1>--Animation-transition--</h1>
+<h1>--Transitions-transition--</h1>
 <section class="section-transition">
 	<div class="pulser-transition"></div>
 </section>
 
-<h1>--Animation-non-linear--</h1>
+<h1>--Transitions-non-linear--</h1>
 <section class="section-non-linear">
 	<div class="pulser-non-linear"></div>
 </section>
 
+<h1>--Animations--</h1>
+<section class="section-animations">
+	<div class="pulser-animations"></div>
+</section>
+
+<h1>--Animations-enhanced--</h1>
+
+<section class="section-animations-enhanced">
+	<div class="pulser-animations-enhanced"></div>
+</section>
+
 <style>
+	/*Animations enhanced*/
+	div.pulser-animations-enhanced {
+		width: 50px;
+		height: 50px;
+		background-color: #09f;
+		border-radius: 50%;
+		position: relative;
+	}
+
+	.pulser-animations-enhanced::after {
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		background: #09f;
+		border-radius: 50%;
+		z-index: -1;
+		/* ----- */
+		animation-name: pulse;
+		animation-duration: 2s;
+		animation-timing-function: ease-in-out;
+		animation-iteration-count: 5;
+	}
+	@keyframes pulse {
+		/* build the animation */
+		0% {
+			opacity: 0;
+		}
+		50% {
+			scale: 1.4;
+			opacity: 0.4;
+		}
+	}
+
+	.section-animations-enhanced {
+		display: grid;
+		place-content: center;
+		min-height: 50vh;
+		border: 4px solid #000;
+	}
+	/*Animations simple*/
+	div.pulser-animations {
+		width: 50px;
+		height: 50px;
+		background-color: #09f;
+		border-radius: 50%;
+		position: relative;
+		animation: move; /* move is the name of the animation */
+		animation-duration: 3s; /* duration of the animation */
+	}
+
+	@keyframes move {
+		/* build the animation */
+		from {
+			transform: translateY(0px);
+		}
+		to {
+			transform: translateY(100px);
+		}
+	}
+
+	.section-animations {
+		display: grid;
+		place-content: center;
+		min-height: 50vh;
+		border: 4px solid #000;
+	}
+
 	/*Non linear*/
 	div.pulser-non-linear {
 		width: 50px;
