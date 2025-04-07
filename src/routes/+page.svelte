63db39bd901d1090ec1083c1b3652d3b56e39a17
ccalobeto <calobeto.leon@gmail.com>
2025-04-06 21:03:07 -0500

    fix load of async data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,67 +1,66 @@
 <script>
-  import projects from "$lib/projects.json";
-  import Project from "$lib/components/Project.svelte";
+	import projects from "$lib/projects.json";
+	import Project from "$lib/components/Project.svelte";
+	import { onMount } from "svelte";
 
-  let promise = fetch("https://api.github.com/users/leaverou").then(
-    (response) => response.json(),
-  );
+	let data;
+
+	onMount(async () => {
+		const response = await fetch("https://api.github.com/users/leaverou");
+		data = await response.json();
+	});
 </script>
 
 <svelte:head>
-  <title>Home</title>
+	<title>Home</title>
 </svelte:head>
 <p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse culpa quas id
-  alias velit libero porro ex sunt fugit quibusdam quasi quaerat reprehenderit
-  consequuntur, earum praesentium itaque, facilis architecto ratione?
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse culpa quas id
+	alias velit libero porro ex sunt fugit quibusdam quasi quaerat reprehenderit
+	consequuntur, earum praesentium itaque, facilis architecto ratione?
 </p>
 <img src="./images/indy.jpg" alt="My favorite person" />
 
-<section>
-  <h2>My Github Stats</h2>
-  {#await promise}
-    <p>Loading...</p>
-  {:then data}
-    <dl>
-      <dt>Followers:</dt>
-      <dd>{data.followers}</dd>
-      <dt>Following:</dt>
-      <dd>{data.following}</dd>
-      <dt>Repos:</dt>
-      <dd>{data.public_repos}</dd>
-      <dt>Gists:</dt>
-      <dd>{data.public_gists}</dd>
-    </dl>
-  {:catch error}
-    <p class="error">
-      Something went wrong: {error.message}
-    </p>
-  {/await}
-</section>
-<h1>
-  Latest Projects
-  <div class="projects">
-    {#each projects.slice(0, 3) as p}
-      <Project data={p} hLevel="3" />
-    {/each}
-  </div>
-</h1>
+{#if data}
+	<section>
+		<h2>My Github Stats</h2>
+
+		<dl>
+			<dt>Followers:</dt>
+			<dd>{data.followers}</dd>
+			<dt>Following:</dt>
+			<dd>{data.following}</dd>
+			<dt>Repos:</dt>
+			<dd>{data.public_repos}</dd>
+			<dt>Gists:</dt>
+			<dd>{data.public_gists}</dd>
+		</dl>
+	</section>
+	<h1>
+		Latest Projects
+		<div class="projects">
+			{#each projects.slice(0, 3) as p}
+				<Project data={p} hLevel="3" />
+			{/each}
+		</div>
+	</h1>
+{/if}
 
 <style>
-  dl {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 10px;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-  }
-  dt {
-    font-weight: bold;
-    grid-row: 1;
-  }
-  dd {
-    margin-left: 0;
-    grid-row: 2;
-  }
+	dl {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-gap: 10px;
+		padding: 10px;
+		border: 1px solid #ccc;
+		border-radius: 5px;
+	}
+	dt {
+		font-weight: bold;
+		grid-row: 1;
+	}
+	dd {
+		margin-left: 0;
+		grid-row: 2;
+	}
 </style>
